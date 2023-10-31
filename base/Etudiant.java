1e3c3508ed56a960b31ae4a5dfff80753f7386d4
adlanemrg <adlanemeraga@MacBook-Air-de-Adlane.local>
2023-10-31 11:29:18 +0100

    version 3 connection bdd

---
+++ base/Etudiant.java
@@ -0,0 +1,47 @@
+package base;
+ 
+public class Etudiant {
+    
+    private int id;
+    private String nom;
+    private String prenom;
+    private String sexe;
+    private Formation formation;
+
+
+    public Etudiant() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getSexe() {
+        return sexe;
+    }
+
+    public void setSexe(String sexe) {
+        this.sexe = sexe;
+    }
+
+}
