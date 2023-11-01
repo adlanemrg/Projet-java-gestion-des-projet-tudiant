1e90de0bba9d8bf17e9f5fc6f7fadd237b728ba5
adlanemrg <adlanemeraga@MacBook-Air-de-Adlane.local>
2023-11-01 13:05:39 +0100

    ajout d'une classe Connection

---
+++ connexion_bdd/Connection.java
@@ -0,0 +1,24 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+        // Informations de connexion à la base de données
+        String url = "jdbc:mysql://localhost:3306/java_project";
+        String utilisateur = "root";
+        String motDePasse = "";
+
+        // Établir la connexion
+        try {
+            Connection connexion = DriverManager.getConnection(url, utilisateur, motDePasse);
+
+            // Votre code ici pour interagir avec la base de données
+
+            // Fermer la connexion lorsque vous avez terminé
+            connexion.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
