8fd62da07fcd60de58a9b26360756b098242ab79
adlanemrg <adlanemeraga@MacBook-Air-de-Adlane.local>
2023-11-01 17:23:34 +0100

    version avec MAVEN ajouté

---
+++ JavaApp/src/main/java/ProjetM1Miage/App.java
@@ -0,0 +1,13 @@
+package ProjetM1Miage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
